d780cb03bcf1edd5fb17de5bd109013459fa74aa
jonathangunnells <jonathan.gunnells@yahoo.com>
2016-06-22 22:21:11 -0400

    Added functionality

--- src/main/java/com/theironyard/Message.java
+++ src/main/java/com/theironyard/Message.java
@@ -1,8 +1,16 @@
 package com.theironyard;
 
+import javax.persistence.*;
 
+@Entity
+@Table(name = "messages")
 public class Message {
+
+    @Id
+    @GeneratedValue
     int id;
+
+    @Column(nullable = false)
     String text;
 
     public Message(int id, String text) {
--- src/main/java/com/theironyard/MicroblogController.java
+++ src/main/java/com/theironyard/MicroblogController.java
@@ -1,5 +1,6 @@
 package com.theironyard;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,28 +11,24 @@ import java.util.ArrayList;
 
 
 @Controller
-
-
 public class MicroblogController {
 
-    static ArrayList<Message> messages = new ArrayList<>();
+    @Autowired
+    MessageRepository messages;
 
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String home(Model model, HttpSession session) {
 
-        for(int i = 0; i < messages.size(); i++) {
-            messages.get(i).id = i+1;
-        }
-       // model.addAttribute("home");
 
         String username = (String) session.getAttribute("username");
         User user = null;
         if (username != null) {
             user = new User(username);
+
         }
         model.addAttribute("user", user);
-        model.addAttribute("messages", messages);
+        model.addAttribute("messages", messages.findAll());
 
         return "home";
 
@@ -50,15 +47,15 @@ public class MicroblogController {
     }
 
     @RequestMapping(path = "/add-message", method = RequestMethod.POST)
-    public String addMessage(String message, HttpSession session){
-        Message msg = new Message(messages.size() + 1, message);
-        messages.add(msg);
+    public String addMessage(int id, String message, HttpSession session){
+        Message msg = new Message(id, message);
+        messages.save(msg);
     return"redirect:/";
 }
 
     @RequestMapping(path = "/delete-message", method = RequestMethod.POST)
     public String deleteMessage(int id) {
-        messages.remove(id - 1);
+        messages.delete(id - 1);
         return "redirect:/";
     }
 
